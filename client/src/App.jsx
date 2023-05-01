afa858090c88a3979df5ecf9fbc6c5d0d8c5fa03
Prasanna Rajendran <imprasannarajendran@gmail.com>
2023-05-01 12:04:53 +0530

    Server Congif, Routes Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import ChatBody from "./Components/ChatBody";
 import ChatInput from "./Components/ChatInput";
 
 function App() {
+  const [chat, setChat] = useState([]);
   return (
     <div
       className="bg-[#1a232e] h-screen py-6 relative sm:px-16 px-12
@@ -12,7 +14,7 @@ function App() {
       <div className="gradient-02 z-0 absolute"></div>
       <div className="font-bold text-2xl text-center mb-3">ChatGPT-UI</div>
       <div className="h-[90%] overflow-auto w-full max-w-4xl min-w-[20rem] py-8 px-4 self-center">
-        <ChatBody />
+        <ChatBody chat={chat} />
       </div>
       <div className="w-full max-w-4xl min-w-[20rem] self-center">
         <ChatInput />
--- server/index.js
+++ server/index.js
@@ -11,8 +11,34 @@ const app = express();
 env.config();
 app.use(cors());
 app.use(bodyParser.json());
+
+const Config = new Configuration({
+  organization: "org-bOagEdfYA6gwQGOXoEYX3rBV",
+  apiKey: process.env.API_KEY,
+});
+
+const openAI = new OpenAIApi(Config);
+
 app.listen("3080", () => console.log("Listening on port 3080"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const response = await openAI.createCompletion({
+      model: "text-devinci-003",
+      prompt: `${message}`,
+      max_tokens: 100,
+      temperature: 0.5,
+    });
+
+    res.json({ message: response.data.choices[0].value }).status(200);
+  } catch (e) {
+    console.log(e);
+    res.send(e).status(400);
+  }
 });
